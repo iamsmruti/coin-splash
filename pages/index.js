c9e719978c60571419e471c75021b75bec8993dc
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2022-05-16 23:34:34 +0530

    cleaned code

--- pages/index.js
+++ pages/index.js
@@ -5,43 +5,10 @@ import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
-  const useWindowDimensions = () => {
-    const hasWindow = typeof window !== "undefined"
-
-    function getWindowDimensions() {
-      const width = hasWindow ? window.innerWidth : null
-      const height = hasWindow ? window.innerHeight : null
-      return {
-        width,
-        height,
-      }
-    }
-
-    const [windowDimensions, setWindowDimensions] = useState(
-      getWindowDimensions()
-    )
-
-    useEffect(() => {
-      if (hasWindow) {
-        function handleResize() {
-          setWindowDimensions(getWindowDimensions())
-        }
-
-        window.addEventListener("resize", handleResize)
-        return () => window.removeEventListener("resize", handleResize)
-      }
-    }, [hasWindow])
-
-    return windowDimensions
-  }
-
-  const { height, width } = useWindowDimensions()
-  const breakpoint = 450
   return (
     <div className={styles.container}>
         <div className={styles.imageContainer}>
-          {width > breakpoint && <Image className={styles.image} src='/favicon.png' width={500} height={500} />}
-          {width <= breakpoint && <Image className={styles.image} src='/favicon.png' width={280} height={280} />}
+          { <Image className={styles.image} src='/favicon.png' width={500} height={500} />}
         </div>
 
         <div className={styles.info}>
